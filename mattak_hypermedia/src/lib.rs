0099f1a274aa3912428f1bb9ff878e7d5cc868cc
Judson Lester <nyarly@gmail.com>
2025-11-11 16:50:57 -0800

    refactor: mattak_hypermedia crate

--- mattak_hypermedia/src/lib.rs
+++ mattak_hypermedia/src/lib.rs
@@ -1,14 +1,332 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use axum::{http, response::IntoResponse};
+use iri_string::template::UriTemplateString;
+pub use iri_string::types::IriReferenceString;
+use serde::Serialize;
+
+// use crate::{error::Error, routing};
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("couldn't validate IRI: {0:?}")]
+    IriValidate(#[from] iri_string::validate::Error),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        use http::status::StatusCode;
+        match self {
+            Error::IriValidate(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+            }
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Serialize, Clone)]
+#[serde(tag = "type")]
+pub enum Affordance {
+    IriTemplate {
+        id: IriReferenceString,
+        template: UriTemplateString,
+        operation: Vec<Operation>,
+    },
+    Link {
+        id: IriReferenceString,
+        operation: Vec<Operation>,
+    },
+}
+
+impl From<IriTemplate> for Affordance {
+    fn from(
+        IriTemplate {
+            id,
+            template,
+            operation,
+        }: IriTemplate,
+    ) -> Self {
+        Self::IriTemplate {
+            id,
+            template,
+            operation,
+        }
+    }
+}
+impl From<Link> for Affordance {
+    fn from(Link { id, operation }: Link) -> Self {
+        Self::Link { id, operation }
+    }
+}
+
+#[derive(Serialize, Clone)]
+#[serde(tag = "type")]
+pub struct IriTemplate {
+    pub id: IriReferenceString,
+    // pub r#type: String,
+    pub template: UriTemplateString,
+    pub operation: Vec<Operation>,
+}
+
+#[derive(Serialize, Clone)]
+#[serde(tag = "type")]
+pub struct Link {
+    pub id: IriReferenceString,
+    // pub r#type: String,
+    pub operation: Vec<Operation>,
+}
+
+#[derive(Default, Serialize, Clone)]
+pub struct Operation {
+    pub method: Method,
+    pub r#type: String,
+}
+
+#[derive(Default, Clone)]
+pub struct Method(http::Method);
+
+impl From<http::Method> for Method {
+    fn from(value: http::Method) -> Self {
+        Self(value)
+    }
+}
+
+impl Serialize for Method {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_str(self.0.as_str())
+    }
+}
+
+#[derive(Default, Serialize, Clone)]
+pub enum ActionType {
+    #[default]
+    #[serde(rename = "ViewAction")]
+    View,
+    #[serde(rename = "CreateAction")]
+    Create,
+    #[serde(rename = "UpdateAction")]
+    Update,
+    #[serde(rename = "FindAction")]
+    Find,
+    #[serde(rename = "AddAction")]
+    Add,
+    // this is not a schema.org Action type
+    #[serde(rename = "LoginAction")]
+    Login,
+    // this is not a schema.org Action type
+    #[serde(rename = "LogoutAction")]
+    Logout,
+}
+
+impl std::fmt::Display for ActionType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ActionType::View => write!(f, "ViewAction"),
+            ActionType::Create => write!(f, "CreateAction"),
+            ActionType::Update => write!(f, "UpdateAction"),
+            ActionType::Find => write!(f, "FindAction"),
+            ActionType::Add => write!(f, "AddAction"),
+            ActionType::Login => write!(f, "LoginAction"),
+            ActionType::Logout => write!(f, "LogoutAction"),
+        }
+    }
+}
+
+/*
+schema.org ActionType:
+(note: because RDF, these cannot be exhaustive)
+
+AchieveAction
+    LoseAction
+    TieAction
+    WinAction
+AssessAction
+    ChooseAction
+        VoteAction
+    IgnoreAction
+    ReactAction
+        AgreeAction
+        DisagreeAction
+        DislikeAction
+        EndorseAction
+        LikeAction
+        WantAction
+    ReviewAction
+ConsumeAction
+    DrinkAction
+    EatAction
+    InstallAction
+    ListenAction
+    PlayGameAction
+    ReadAction
+    UseAction
+        WearAction
+    ViewAction
+    WatchAction
+ControlAction
+    ActivateAction
+    DeactivateAction
+    ResumeAction
+    SuspendAction
+CreateAction
+    CookAction
+    DrawAction
+    FilmAction
+    PaintAction
+    PhotographAction
+    WriteAction
+FindAction
+    CheckAction
+    DiscoverAction
+    TrackAction
+InteractAction
+    BefriendAction
+    CommunicateAction
+        AskAction
+        CheckInAction
+        CheckOutAction
+        CommentAction
+        InformAction
+            ConfirmAction
+            RsvpAction
+        InviteAction
+        ReplyAction
+        ShareAction
+    FollowAction
+    JoinAction
+    LeaveAction
+    MarryAction
+    RegisterAction
+    SubscribeAction
+    UnRegisterAction
+MoveAction
+    ArriveAction
+    DepartAction
+    TravelAction
+OrganizeAction
+    AllocateAction
+        AcceptAction
+        AssignAction
+        AuthorizeAction
+        RejectAction
+    ApplyAction
+    BookmarkAction
+    PlanAction
+        CancelAction
+        ReserveAction
+        ScheduleAction
+PlayAction
+    ExerciseAction
+    PerformAction
+SearchAction
+SeekToAction
+SolveMathAction
+TradeAction
+    BuyAction
+    OrderAction
+    PayAction
+    PreOrderAction
+    QuoteAction
+    RentAction
+    SellAction
+    TipAction
+TransferAction
+    BorrowAction
+    DonateAction
+    DownloadAction
+    GiveAction
+    LendAction
+    MoneyTransfer
+    ReceiveAction
+    ReturnAction
+    SendAction
+    TakeAction
+UpdateAction
+    AddAction
+        InsertAction
+            AppendAction
+            PrependAction
+    DeleteAction
+    ReplaceAction
+
+* */
+
+/// op is used to create a most-common operation for each action type
+pub fn op(action: ActionType) -> Operation {
+    use axum::http::Method;
+    use ActionType::*;
+    match action {
+        View => Operation {
+            method: Method::GET.into(),
+            r#type: action.to_string(),
+        },
+        Create => Operation {
+            method: Method::PUT.into(),
+            r#type: action.to_string(),
+        },
+        Update => Operation {
+            method: Method::PUT.into(),
+            r#type: action.to_string(),
+        },
+        Find => Operation {
+            method: Method::GET.into(),
+            r#type: action.to_string(),
+        },
+        Add => Operation {
+            method: Method::POST.into(),
+            r#type: action.to_string(),
+        },
+        Login => Operation {
+            method: Method::POST.into(),
+            r#type: action.to_string(),
+        },
+        Logout => Operation {
+            method: Method::DELETE.into(),
+            r#type: action.to_string(),
+        },
+    }
+}
+
+const RESOURCE: &str = "Resource";
+
+#[derive(Serialize, Clone)]
+pub struct ResourceType(&'static str);
+
+impl Default for ResourceType {
+    fn default() -> Self {
+        Self(RESOURCE)
+    }
+}
+
+#[derive(Serialize, Clone)]
+pub struct ResourceFields<L: Serialize + Clone> {
+    pub id: IriReferenceString,
+    pub r#type: ResourceType,
+    pub operation: Vec<Operation>,
+    pub find_me: IriTemplate,
+    pub nick: L,
+}
+
+// XXX interface changed because we can't have a dep-loop
+// between Routing and Hypermedia
+// TODO add a complementary function/method in Routing
+impl<L: Serialize + Clone + Serialize> ResourceFields<L> {
+    pub fn new(
+        id: IriReferenceString,
+        template: UriTemplateString,
+        nick: L,
+        api_name: &str,
+        operation: Vec<Operation>,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            id,
+            nick,
+            operation,
+            r#type: Default::default(),
+            find_me: IriTemplate {
+                template,
+                id: api_name.try_into()?,
+                operation: vec![op(ActionType::Find)],
+            },
+        })
     }
 }
