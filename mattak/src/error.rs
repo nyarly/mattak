5a63759c46ba6313f902ba7d7776ed146c3b01a6
Judson Lester <nyarly@gmail.com>
2025-11-11 18:58:34 -0800

    refactor: feature selection for sub-crates
    Only use the parts you need

--- mattak/src/error.rs
+++ mattak/src/error.rs
@@ -1,37 +1,45 @@
 use axum::{http, response::IntoResponse};
 use tracing::debug;
 
-use crate::{biscuits, condreq, hypermedia, querymapping, routing};
-
 #[derive(thiserror::Error, Debug)]
 #[non_exhaustive]
 pub enum Error {
     #[error("unknown error: {0}")]
     Unknown(String),
     #[error("database error: {0}")]
-    Querymapping(#[from] querymapping::Error),
+    #[cfg(feature = "querymapping")]
+    Querymapping(#[from] crate::querymapping::Error),
     #[error("authentication: {0}")]
-    Biscuits(#[from] biscuits::Error),
+    #[cfg(feature = "biscuits")]
+    Biscuits(#[from] crate::biscuits::Error),
     // #[error("cachecontrol: {0:?}")]
     // Cachecontrol(#[from] cachecontrol::Error),
     #[error("condreq: {0:?}")]
-    Condreq(#[from] condreq::Error),
+    #[cfg(feature = "condreq")]
+    Condreq(#[from] crate::condreq::Error),
     #[error("hypermedia: {0:?}")]
-    Hypermedia(#[from] hypermedia::Error),
+    #[cfg(feature = "hypermedia")]
+    Hypermedia(#[from] crate::hypermedia::Error),
     // #[error("ratelimiting: {0:?}")]
     // Ratelimiting(#[from] ratelimiting::Error),
     #[error("routing: {0:?}")]
-    Routing(#[from] routing::Error),
+    #[cfg(feature = "routing")]
+    Routing(#[from] crate::routing::Error),
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         debug!("Returning error: {:?}", &self);
         match self {
+            #[cfg(feature = "biscuits")]
             Error::Biscuits(e) => e.into_response(),
+            #[cfg(feature = "querymapping")]
             Error::Querymapping(e) => e.into_response(),
+            #[cfg(feature = "condreq")]
             Error::Condreq(e) => e.into_response(),
+            #[cfg(feature = "hypermedia")]
             Error::Hypermedia(e) => e.into_response(),
+            #[cfg(feature = "routing")]
             Error::Routing(e) => e.into_response(),
             Error::Unknown(_) => {
                 (http::StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
--- mattak/src/lib.rs
+++ mattak/src/lib.rs
@@ -1,10 +1,17 @@
 pub mod error;
 pub use error::Error;
 
+#[cfg(feature = "biscuits")]
 pub use mattak_biscuits as biscuits;
+#[cfg(feature = "cachecontrol")]
 pub use mattak_cachecontrol as cachecontrol;
+#[cfg(feature = "condreq")]
 pub use mattak_condreq as condreq;
+#[cfg(feature = "hypermedia")]
 pub use mattak_hypermedia as hypermedia;
+#[cfg(feature = "querymapping")]
 pub use mattak_querymapping as querymapping;
+#[cfg(feature = "querymapping")]
 pub use mattak_ratelimiting as ratelimiting;
+#[cfg(feature = "routing")]
 pub use mattak_routing as routing;
